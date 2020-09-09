d2a72bb5e26739d04f234eec702c35939771ce62
josuecx <2994925750101@ingenieria.usac.edu.gt>
2020-09-09 01:56:18 -0600

    corregion nav principal

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -16,8 +16,8 @@ namespace otello
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-            //            "~/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
@@ -33,9 +33,11 @@ namespace otello
                       "~/Content/bootstrap.min.css,map",
                       "~/Content/estiloprin.css",
                       "~/Content/Site.css"));
+           
             bundles.Add(new StyleBundle("~/Content/slate").Include(
                 "~/Content/bootstrap-slate.css",
                 "~/Content/estiloprin.css",
+                "~/Content/bootstrap-slate.min.css",
                 "~/Content/site.css"
                 ));
         }
